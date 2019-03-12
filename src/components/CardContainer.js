ac65f4f1baf8f11f154e06376d8f08d43d5f2e04
Renan Costa <Renan.Costa@ltm.digital>
2019-03-12 00:09:41 -0300

    Pre select card

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
-import Card from './Card'
+import Card from "./Card"
 
-import { PlanningPoker } from '../cards'
+import { PlanningPoker } from "../cards"
 
 class CardContainer extends Component {
   constructor(props) {
     super(props)
-    this.state = { value: '', image: '', isSelected: true }
+    this.state = { value: "", image: "", isSelected: true }
   }
 
   componentDidMount() {
-    const { match: { params: { cardValue } } } = this.props
-    const selectedCard = PlanningPoker.find(card => card.value === cardValue) || {}
+    const {
+      match: {
+        params: { cardValue }
+      }
+    } = this.props
+    const selectedCard =
+      PlanningPoker.find(card => card.value === cardValue) || {}
     this.setState({ ...selectedCard })
   }
 
   render() {
-    return <Card {...this.state} />
+    return (
+      <Link to="/">
+        <Card {...this.state} />
+      </Link>
+    )
   }
 }
 
