6d96b7d88a3e70d25da005487b9c20016344cbeb
Emerson Soares <on.emersonsoares@gmail.com>
2019-05-04 23:58:34 -0300

    fixing basename path

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './components/App'
 
 const app = (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 )
